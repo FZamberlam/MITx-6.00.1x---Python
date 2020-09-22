3673af6e8fb805256bb51f8ae9fa73d2cd03c8ce
Felipe Zamberlam <fzamberlam@gmail.com>
2020-09-22 04:30:47 -0300

    fixed random word call

--- Week_3_-_Structured_Types/Problem_Set_3/ps3_hangman.py
+++ Week_3_-_Structured_Types/Problem_Set_3/ps3_hangman.py
@@ -152,6 +152,5 @@ def hangman(secretWord):
 # and run this file to test! (hint: you might want to pick your own
 # secretWord while you're testing)
 
-# secretWord = chooseWord(wordlist).lower()
-secretWord = 'else'
+secretWord = chooseWord(wordlist).lower()
 hangman(secretWord)
