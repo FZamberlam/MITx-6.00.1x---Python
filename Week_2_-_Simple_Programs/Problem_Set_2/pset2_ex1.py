7d0a49bea0bba9d0a776e7d3251e1faee519e8af
Felipe Zamberlam <fzamberlam@gmail.com>
2020-09-22 06:24:16 -0300

    Add files via upload

--- Week_2_-_Simple_Programs/Problem_Set_2/pset2_ex1.py
+++ Week_2_-_Simple_Programs/Problem_Set_2/pset2_ex1.py
@@ -5,34 +5,60 @@ Created on Fri Sep 11 19:47:45 2020
 @author: Zamberlam
 """
 
-"""
+# Write a program to calculate the credit card balance after one year if a 
+# person only pays the minimum monthly payment required by the credit card 
+# company each month.
 
-Balance = Unpaid + Interest
-Minimum = Balance * 0.02
-Unpaid = Balance - Minimum
-Interest = 0.18/12.0 * Unpaid
+# The following variables contain values as described below:
 
-Month    Balance    Minimum    Unpaid      Interest
-0        5000.00    100        4900        73.50
-1        4973.50    99.47      4874.03     73.11
-2        4947.14    98.94      4848.20     72.72
+#   balance - the outstanding balance on the credit card
+#   annualInterestRate - annual interest rate as a decimal
+#   monthlyPaymentRate - minimum monthly payment rate as a decimal
 
-"""
+# For each month, calculate statements on the monthly payment and remaining 
+# balance. At the end of 12 months, print out the remaining balance. Be sure 
+# to print out no more than two decimal digits of accuracy - so print
+
+# Remaining balance: 813.41
+
+# instead of
+
+# Remaining balance: 813.4141998135 
+
+# So your program only prints out one thing: the remaining balance at the end 
+# of the year in the format:
+
+# Remaining balance: 4784.0
+
+# A summary of the required math is found below:
+
+#   Monthly interest rate= (Annual interest rate) / 12.0
+#   Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+#   Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+#   Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+
+def minimumPaymentBalance(balance, annualInterestRate, monthlyPaymentRate):
+    '''
+    balance: float or int
+    annualInterestRate: float or int
+    monthlyPaymentRate: float or int
+    returns: float, remaining balance after a year paying only the minimum
+    '''
+    monthlyInterestRate = annualInterestRate / 12.0
+    month = 1
+
+    while month <= 12:
+        
+        minimumMonthlyPayment = monthlyPaymentRate * balance
+        monthlyUnpaidBalance = balance - minimumMonthlyPayment
+        balance = monthlyUnpaidBalance + (monthlyInterestRate * monthlyUnpaidBalance)
+        
+        month += 1
+        
+    return round(balance, 2)
 
 balance = 42
 annualInterestRate = 0.2
 monthlyPaymentRate = 0.04
 
-monthlyInterestRate = annualInterestRate / 12.0
-
-month = 1
-
-while month <= 12:
-    
-    minimumMonthlyPayment = monthlyPaymentRate * balance
-    monthlyUnpaidBalance = balance - minimumMonthlyPayment
-    balance = monthlyUnpaidBalance + (monthlyInterestRate * monthlyUnpaidBalance)
-    
-    month += 1
-    
-print("Remaining balance:", round(balance, 2))
+print("Remaining balance:", minimumPaymentBalance(balance, annualInterestRate, monthlyPaymentRate))
--- Week_2_-_Simple_Programs/Problem_Set_2/pset2_ex2.py
+++ Week_2_-_Simple_Programs/Problem_Set_2/pset2_ex2.py
@@ -3,44 +3,63 @@
 Created on Sat Sep 12 00:08:03 2020
 
 @author: Zamberlam
-"""
+"""  
 
-balance = 4773
-annualInterestRate = 0.2    
+# Now write a program that calculates the minimum fixed monthly payment needed
+# in order pay off a credit card balance within 12 months. By a fixed monthly 
+# payment, we mean a single number which does not change each month, but 
+# instead is a constant amount that will be paid each month.
+
+# In this problem, we will not be dealing with a minimum monthly payment rate.
+
+# The following variables contain values as described below:
+
+#   balance - the outstanding balance on the credit card
+#   annualInterestRate - annual interest rate as a decimal
+
+# The program should print out one line: the lowest monthly payment that will 
+# pay off all debt in under 1 year, for example:
+
+# Lowest Payment: 180 
+
+# Assume that the interest is compounded monthly according to the balance at 
+# the end of the month (after the payment for that month is made). The monthly
+# payment must be a multiple of $10 and is the same for all months. Notice 
+# that it is possible for the balance to become negative using this payment 
+# scheme, which is okay. A summary of the required math is found below:
+
+#   Monthly interest rate = (Annual interest rate) / 12.0
+#   Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
+#   Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
     
 def lowestPayment(balance, annualInterestRate, minimumMonthlyPayment = 10):
-    
     '''
     balance: int or float
     annualInterestRate: float
     minimumMonthlyPayment: int, if not specified, default value is 10
-    
     return minimumMonthlyPayment to pay off debt in a year
     '''
-    
     temp = balance
     month = 1
-    
     monthlyInterestRate = annualInterestRate / 12.0
     
-    while month <= 12:
-        
+    while month <= 12: 
         balance = round(balance, 2)
-        
         monthlyUnpaidBalance = balance - minimumMonthlyPayment
         balance = monthlyUnpaidBalance + (monthlyInterestRate * monthlyUnpaidBalance)
         
         month += 1
         
     if monthlyUnpaidBalance <= 0:
-        
         return minimumMonthlyPayment
     
-    else:
-        
+    else: 
         balance = temp
         return lowestPayment(balance, annualInterestRate, minimumMonthlyPayment + 10)
+    
+balance = 3329
+annualInterestRate = 0.2  
       
-print(lowestPayment(balance, annualInterestRate))
+print("Lowest Payment:", lowestPayment(balance, annualInterestRate))
     
     
